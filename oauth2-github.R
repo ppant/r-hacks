ac97a3b185bb77dac4e472ba324c1a3595256694
Pradeep K. Pant <ppant@cpan.org>
2016-03-17 22:20:28 +0530

    Added comments and priting stuff

--- oauth2-github.R
+++ oauth2-github.R
@@ -1,3 +1,4 @@
+## Load required modules 
 library(httr)
 library(httpuv)
 require(jsonlite)
@@ -8,8 +9,11 @@ require(jsonlite)
           oauth_endpoints("github")
         
         # 2. To make your own application, register at at 
-        #    https://github.com/settings/applications. Use any URL for the homepage URL
-        #    (http://github.com is fine) and  http://localhost:1410 as the callback url
+        #    https://github.com/settings/applications. 
+        ##  https://github.com/settings/applications/321837
+        ## Use any URL for the homepage URL
+        #    (http://github.com is fine) and  http://localhost:1410 as the callback url. You will need httpuv
+          
         #
         #    Replace your key and secret below.
         myapp <- oauth_app("github",
@@ -18,6 +22,7 @@ require(jsonlite)
 
         # 3. Get OAuth credentials
         github_token <- oauth2.0_token(oauth_endpoints("github"), myapp)
+        ## Authentication will be done automatically
         
         # 4. Use API
         gtoken <- config(token = github_token)
@@ -25,9 +30,12 @@ require(jsonlite)
         stop_for_status(req)
         ##content(req)
         output <- content(req)
+         ## Either of the two can be used to fetch the required info, name and date created 
         out<-list(output[[8]]$name, output[[8]]$created_at)
+        
         BROWSE("https://api.github.com/users/jtleek/repos",authenticate("Access Token","x-oauth-basic","basic"))
         # OR:
         # req <- with_config(gtoken, GET("https://api.github.com/users/jtleek/repos"))
         # stop_for_status(req)
         # content(req)
+        
